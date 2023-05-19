5a1201fe6069e7e962ff0ec286a31700215084b3
eric49861 <1585416826@qq.com>
2023-05-19 23:00:34 +0800

    学习小组展示组件

---
+++ src/components/display/StudyGroup.vue
@@ -0,0 +1,95 @@
+<template>
+    <el-descriptions
+        class="margin-top"
+        title=""
+        :column="3"
+        :size="size"
+        border>
+        <template #extra>
+          <el-button type="primary">进入小组</el-button>
+        </template>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <user />
+              </el-icon>
+              小组名称
+            </div>
+          </template>
+          {{title}}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <iphone />
+              </el-icon>
+              开始时间
+            </div>
+          </template>
+          {{startTime}}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <location />
+              </el-icon>
+              结束时间
+            </div>
+          </template>
+          {{endTime}}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <tickets />
+              </el-icon>
+              Remarks
+            </div>
+          </template>
+          <el-tag size="small">{{type}}</el-tag>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <office-building />
+              </el-icon>
+              学习目标
+            </div>
+          </template>
+          {{goal}}
+        </el-descriptions-item>
+      </el-descriptions>
+</template>
+
+<script>
+
+export default {
+    name: "StudyGroup",
+    props: {
+        title: String,
+        topic: String,
+        startTime: String,
+        endTime: String,
+        type: String,
+        goal:String
+    }
+}
+</script>
+
+<style scoped>
+.el-descriptions {
+  margin-top: 20px;
+}
+.cell-item {
+  display: flex;
+  align-items: center;
+}
+.margin-top {
+  margin-top: 0px;
+}
+</style>
