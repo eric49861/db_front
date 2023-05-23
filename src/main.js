216a0d623e792db3ab23f9b4949e225e1791e4a6
eric49861 <1585416826@qq.com>
2023-05-23 21:38:48 +0800

    添加新的中间件，mock,vuex

--- src/main.js
+++ src/main.js
@@ -10,10 +10,14 @@ import vuetyped from 'vue3typed'
 
 import router from './router/router'
 
+//导入状态管理state
+import store from './store/index'
+
+
 const app = createApp(App)
 app.use(ElementPlus)
-app.use(router)
-app.use(vuetyped)
+    .use(router)
+    .use(vuetyped).use(store)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
