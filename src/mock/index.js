cb884f3fd06e3bf74500f27f65d5f6f72d21d884
eric49861 <1585416826@qq.com>
2023-05-24 15:17:49 +0800

    模拟后台数据

---
+++ src/mock/index.js
@@ -0,0 +1,8 @@
+//配置mockjs
+import mockjs from "mockjs";
+import service from "./mockData/data"
+
+mockjs.mock('/v1/getGroup/ing', service.getIngGroup)
+mockjs.mock('/v1/getGroup/ed', service.getEdGroup)
+mockjs.mock("/v1/getGroup/all", service.getAllGroup)
+mockjs.mock("/v1/getGroup/mine", service.getMyGroup)
---
+++ src/mock/mockData/data.js
@@ -0,0 +1,110 @@
+export default {
+    getIngGroup: () => {
+        return {
+            code: 200,
+            groups: [
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                },
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                }
+            ]
+        }
+    },
+    getEdGroup: () => {
+        return {
+            code: 200,
+            groups: [
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                },
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                }
+            ]
+        }
+    },
+    getAllGroup: () => {
+        return {
+            code: 200,
+            groups: [
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                },
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                }
+            ]
+        }
+    },
+    getMyGroup: () => {
+        return {
+            code: 200,
+            groups: [
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                },
+                {
+                    owner: "Eric",
+                    name: "7号",
+                    remarks: "cs",
+                    startTime: "2023/05/21",
+                    endTime: "2023/05/21",
+                    state: 0,
+                    topic: "computer",
+                    notice: "公告"
+                }
+            ]
+        }
+    }
+}
