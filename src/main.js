ee264d081db606cdfe1eefb344b79f6697b812f4
eric49861 <1585416826@qq.com>
2023-05-24 15:21:37 +0800

    引入mockjs

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import vuetyped from 'vue3typed'
 
 import router from './router/router'
-
+import './mock/index.js'
 //导入状态管理state
 import store from './store/index'
 
@@ -17,7 +17,8 @@ import store from './store/index'
 const app = createApp(App)
 app.use(ElementPlus)
     .use(router)
-    .use(vuetyped).use(store)
+    .use(vuetyped)
+    .use(store)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
