d31822ecad9ad422906ac989256d42a92d6961a4
eric49861 <1585416826@qq.com>
2023-05-24 15:17:30 +0800

    后台接口

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+import service from './index'
+
+export default {
+    getIngGroup: () => {
+        return service({
+            url: "v1/getGroup/ing",
+            methods: "GET"
+        })
+    },
+    getEdGroup: () => {
+        return service({
+            url: "v1/getGroup/ed",
+            methods: "GET"
+        })
+    },
+    getAllGroup: () => {
+        return service({
+            url: "v1/getGroup/all",
+            methods: "GET"
+        })
+    },
+    getMyGroup: () => {
+        return service({
+            url: "v1/getGroup/mine",
+            methods: "GET"
+        })
+    }
+}
+
+
