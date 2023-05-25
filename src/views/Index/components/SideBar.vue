0ccf9be208697a5f67afcb6fb1476afa503d0417
eric49861 <1585416826@qq.com>
2023-05-25 19:57:07 +0800

    绑定侧边栏中头像的地址和实际用户的一致

--- src/views/Index/components/SideBar.vue
+++ src/views/Index/components/SideBar.vue
@@ -4,7 +4,7 @@
             <div class="avatar" v-show="!isCollapse">
                 <el-avatar :size="60" src="https://empty" @error="errorHandler">
                     <img
-                        src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
+                        :src="$store.getters.getUser.avatar"
                     />
                 </el-avatar>
             </div>
