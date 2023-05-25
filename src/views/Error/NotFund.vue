6bde4c27fa9a9ec8e1ba68cca873dd35b5eeb791
eric49861 <1585416826@qq.com>
2023-05-25 15:34:03 +0800

    添加错误页面

---
+++ src/views/Error/NotFund.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="not-found">
+        <el-card>
+            <div class="title">404</div>
+            <div class="description">页面未找到</div>
+            <div class="back-home">
+                <el-button type="primary" @click="goHome">返回首页</el-button>
+            </div>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        goHome() {
+            this.$router.push("/")
+        },
+    },
+};
+</script>
+
+<style scoped>
+.not-found {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+.title {
+    font-size: 80px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.description {
+    font-size: 24px;
+    text-align: center;
+    margin-bottom: 40px;
+}
+
+.back-home {
+    text-align: center;
+}</style>
