b293b4607ac22c79cf63546ccb14522a95c29e91
eric49861 <1585416826@qq.com>
2023-05-25 19:56:10 +0800

    添加用户数据状态管理

--- src/api/index.js
+++ src/api/index.js
@@ -16,7 +16,8 @@ const service = axios.create({
 })
 
 service.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
+    //TODO:从store中获取token添加到请求头中
+    
     return config;
   }, function (error) {
     // 对请求错误做些什么
@@ -25,8 +26,7 @@ service.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 service.interceptors.response.use(function (response) {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
+    //TODO:从请求头中获取token字段，并将该字段放入Vuex中管理，以后发送的请求都要带上该token
     return response;
   }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,25 @@ import { createStore } from "vuex";
 const store = createStore({
     state() {
         return {
-
+            user: {
+                
+            }
+        }
+    },
+    getters: {
+        getUser(state) {
+            return state.user
         }
     },
     mutations: {
-        
+        updateUser(state, user) {
+            state.user = user
+        }
+    },
+    actions: {
+        asyncUpdateUser(context, user) {
+            context.commit("updateUser", user)
+        }
     }
 })
 
