ee49a0648858b6ff1738c6959a25b92cf2048d2b
eric <15854168262@qq.com>
2023-05-30 00:05:48 +0800

    不再使用mockjs模拟数据

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,9 +1,9 @@
 //配置mockjs
-import mockjs from "mockjs";
-import service from "./mockData/data"
+// import mockjs from "mockjs";
+// import service from "./mockData/data"
 
-mockjs.mock('/v1/getGroup/ing', service.getIngGroup)
-mockjs.mock('/v1/getGroup/ed', service.getEdGroup)
-mockjs.mock("/v1/getGroup/all", service.getAllGroup)
-mockjs.mock("/v1/getGroup/mine", service.getMyGroup)
+// mockjs.mock('/v1/getGroup/ing', service.getIngGroup)
+// mockjs.mock('/v1/getGroup/ed', service.getEdGroup)
+// mockjs.mock("/v1/getGroup/all", service.getAllGroup)
+// mockjs.mock("/v1/getGroup/mine", service.getMyGroup)//
 // mockjs.mock("/v1/1/login", "post", service.login)
--- src/mock/mockData/data.js
+++ src/mock/mockData/data.js
@@ -1,133 +1,133 @@
-export default {
-    getIngGroup: () => {
-        return {
-            code: 200,
-            groups: [
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                },
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                }
-            ]
-        }
-    },
-    getEdGroup: () => {
-        return {
-            code: 200,
-            groups: [
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                },
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                }
-            ]
-        }
-    },
-    getAllGroup: () => {
-        return {
-            code: 200,
-            groups: [
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                },
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                }
-            ]
-        }
-    },
-    getMyGroup: () => {
-        return {
-            code: 200,
-            groups: [
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                },
-                {
-                    owner: "Eric",
-                    name: "7号",
-                    remarks: "cs",
-                    imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
-                    startTime: "2023/05/21",
-                    endTime: "2023/05/21",
-                    state: 0,
-                    topic: "computer",
-                    notice: "公告"
-                }
-            ]
-        }
-    },
-    login: () => {
-        return {
-            code: 200,
-            user: {
-                id: 1,
-                name: "HELLO WORLD",
-                password: "85208520",
-                avatar: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
-                QQ: "1585416826",
-                createTime: "2023/05/01 0:0:0",
-                updateTime: "2023/05/01 0:0:0",
-                integral: 100
-            }
-        }
-    }
-}
+// export default {
+//     getIngGroup: () => {
+//         return {
+//             code: 200,
+//             groups: [
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 },
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 }
+//             ]
+//         }
+//     },
+//     getEdGroup: () => {
+//         return {
+//             code: 200,
+//             groups: [
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 },
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 }
+//             ]
+//         }
+//     },
+//     getAllGroup: () => {
+//         return {
+//             code: 200,
+//             groups: [
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 },
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 }
+//             ]
+//         }
+//     },
+//     getMyGroup: () => {
+//         return {
+//             code: 200,
+//             groups: [
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 },
+//                 {
+//                     owner: "Eric",
+//                     name: "7号",
+//                     remarks: "cs",
+//                     imgLink: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+//                     startTime: "2023/05/21",
+//                     endTime: "2023/05/21",
+//                     state: 0,
+//                     topic: "computer",
+//                     notice: "公告"
+//                 }
+//             ]
+//         }
+//     },
+//     login: () => {
+//         return {
+//             code: 200,
+//             user: {
+//                 id: 1,
+//                 name: "HELLO WORLD",
+//                 password: "85208520",
+//                 avatar: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
+//                 QQ: "1585416826",
+//                 createTime: "2023/05/01 0:0:0",
+//                 updateTime: "2023/05/01 0:0:0",
+//                 integral: 100
+//             }
+//         }
+//     }
+// }
